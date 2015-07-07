b864c61bba96f6a6ab1f07f88c27da7110d54e37
gawindx <decauxnico@gmail.com>
2015-07-07 17:04:35 +0200

    more code

--- gd_bakfile.sh
+++ gd_bakfile.sh
@@ -7,8 +7,6 @@
 
 
 set -e
-BAKDIR="/home/e-smith/files/ibays/site_web/html/wp-content/backupwordpress-63ba4a4eed-backups/"
-UPLOAD_DIR_ID=""
 
 function prettyjson_test {
 #Convert JSON data to human-readable form.
@@ -40,23 +38,25 @@ CLIENT_SECRET=""
 SCOPE=${SCOPE:-"https://docs.google.com/feeds"}
 
 if [ "$1" == "create" ]; then
-        local RESPONSE=`curl --silent "https://accounts.google.com/o/oauth2/device/code" --data "client_id=$CLIENT_ID&scope=$SCOPE"`
-        DEVICE_CODE=`echo $RESPONSE | prettyjson | grep 'device_code' | cut -d ':' -f 2 | sed 's/"//g' | sed 's/,//g' | sed 's/\ //g'`
-        USER_CODE=`echo "$RESPONSE" | prettyjson | grep 'user_code' | cut -d ':' -f 2 | sed 's/"//g' | sed 's/,//g' | sed 's/\ //g'`
-        URL=`echo "$RESPONSE" | prettyjson | grep 'verification_url' | cut -d ':' -f 2 | sed 's/"//g' | sed 's/\ //g'`
-        URL=$URL':'`echo "$RESPONSE" | prettyjson | grep 'verification_url' | cut -d ':' -f 3 | sed 's/"//g' | sed 's/\ //g'`
-        echo -n "Go to $URL and enter $USER_CODE to grant access to this application. Hit enter when done..."
-        read
-        local RESPONSE=`curl --silent "https://accounts.google.com/o/oauth2/token" --data "client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET&code=$DEVICE_CODE&grant_type=http://oauth.net/grant_type/device/1$
-        ACCESS_TOKEN=`echo "$RESPONSE" | prettyjson | grep 'access_token' | cut -d ',' -f 1 | cut -d ':' -f 2 | sed 's/"//g' | sed 's/\ //g'`
-        REFRESH_TOKEN=`echo "$RESPONSE" | prettyjson | grep 'refresh_token' | cut -d ',' -f 1 | cut -d ':' -f 2 | sed 's/"//g' | sed 's/\ //g'`
+	
+	local RESPONSE=`curl --silent "https://accounts.google.com/o/oauth2/device/code" --data "client_id=$CLIENT_ID&scope=$SCOPE"`
+	local DEVICE_CODE=`echo $RESPONSE | prettyjson | grep 'device_code' | cut -d ':' -f 2 | sed 's/"//g' | sed 's/,//g' | sed 's/\ //g'`
+	local USER_CODE=`echo "$RESPONSE" | prettyjson | grep 'user_code' | cut -d ':' -f 2 | sed 's/"//g' | sed 's/,//g' | sed 's/\ //g'`
+	local URL=`echo "$RESPONSE" | prettyjson | grep 'verification_url' | cut -d ':' -f 2 | sed 's/"//g' | sed 's/\ //g'`
+	URL=$URL':'`echo "$RESPONSE" | prettyjson | grep 'verification_url' | cut -d ':' -f 3 | sed 's/"//g' | sed 's/\ //g'`
+	local TOKEN_TTL=`echo "$RESPONSE" | prettyjson | grep 'expires_in' | cut -d ':' -f 3 | sed 's/"//g' | sed 's/\ //g'`
+	echo -n "Go to $URL and enter $USER_CODE to grant access to this application. Hit enter when done..."
+	read -t $TOKEN_TTL ENTER_KEY
+	ENTER_KEY=${ENTER_KEY:-timeout}
+	if [ $ENTER_KEY == "timeout" ]; then exit 1
+	RESPONSE=`curl --silent "https://accounts.google.com/o/oauth2/token" --data "client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET&code=$DEVICE_CODE&grant_type=http://oauth.net/grant_type/device/1.0"`
+	ACCESS_TOKEN=`echo "$RESPONSE" | prettyjson | grep 'access_token' | cut -d ',' -f 1 | cut -d ':' -f 2 | sed 's/"//g' | sed 's/\ //g'`
+	REFRESH_TOKEN=`echo "$RESPONSE" | prettyjson | grep 'refresh_token' | cut -d ',' -f 1 | cut -d ':' -f 2 | sed 's/"//g' | sed 's/\ //g'`
 elif [ "$1" == "refresh" ]; then
-        REFRESH_TOKEN=$2
+        REFRESH_TOKEN="$2"
         local RESPONSE=`curl --silent "https://accounts.google.com/o/oauth2/token" --data "client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET&refresh_token=$REFRESH_TOKEN&grant_type=refresh_token"`
         ACCESS_TOKEN=`echo $RESPONSE | prettyjson | grep 'access_token' | cut -d ',' -f 1 | cut -d ':' -f 2 | sed 's/"//g' | sed 's/\ //g'`
 fi
-
-
 }
 
 function upload {
@@ -89,35 +89,49 @@ REFRESH_TOKEN=${REFRESH_TOKEN}
 EOL
 }
 
-function gd_get_file_info {
-GD_FILE=$1
-local RESPONSE=`curl --silent "https://www.googleapis.com/drive/v2/files/$GD_FILE" --header "Authorization: Bearer $ACCESS_TOKEN"`
-FILE_NAME=`echo "$RESPONSE" | prettyjson | grep "title" | cut -d '"' -f 4`
-FILE_SIZE=`echo "$RESPONSE" | prettyjson | grep "fileSize" | cut -d '"' -f 4`
-ARRAY_GD_FILE=($GD_FILE $FILE_NAME $FILE_SIZE)
-echo ${ARRAY_GD_FILE[*]}
+function gd_delete_file {
+local DEL_ID="$1"
+echo "delete file ID : "$DEL_ID
+local RESPONSE=`curl --silent -X "DELETE" "https://www.googleapis.com/drive/v2/files/$DEL_ID" --header "Authorization: Bearer $ACCESS_TOKEN"`
 }
 
-function gd_get_existing_file {
+function gd_get_file_info {
+local GD_FILE_ID="$1"
+local RESPONSE=`curl --silent "https://www.googleapis.com/drive/v2/files/$GD_FILE" --header "Authorization: Bearer $ACCESS_TOKEN"`
+local GD_FILE_NAME=`echo "$RESPONSE" | prettyjson | grep "title" | cut -d '"' -f 4`
+local GD_FILE_SIZE=`echo "$RESPONSE" | prettyjson | grep "fileSize" | cut -d '"' -f 4`
+local ARRAY_GD_INFO_FILE=("$GD_FILE_ID" "$GD_FILE_NAME" "$GD_FILE_SIZE")
+echo ${ARRAY_GD_INFO_FILE[*]}
+}
+
+function gd_get_existing_file_list {
 GD_ARRAY_FILE=()
 local RESPONSE=`curl --silent "https://www.googleapis.com/drive/v2/files/$UPLOAD_DIR_ID/children?q=trashed%3Dfalse" --header "Authorization: Bearer $ACCESS_TOKEN"`
-GD_FILE_LIST=`echo "$RESPONSE" | prettyjson | grep "id" | cut -d '"' -f 4`
-count_index=0
+local GD_FILE_LIST=`echo "$RESPONSE" | prettyjson | grep "id" | cut -d '"' -f 4`
+local count_index=0
+local MY_ARRAY
 for file_id in $GD_FILE_LIST; do
-        MY_ARRAY='id'$count_index'=('$(gd_get_file_info $file_id)')'
+        MY_ARRAY='gd_id'$count_index'=('$(gd_get_file_info $file_id)')'
         GD_ARRAY_FILE=("${GD_ARRAY_FILE[@]}" "$MY_ARRAY")
         count_index=`expr $count_index + 1`
 done
 }
 
-function gd_delete_file {
-local DEL_ID=$1
-echo "delete file ID : "$DEL_ID
-local RESPONSE=`curl --silent -X "DELETE" "https://www.googleapis.com/drive/v2/files/$DEL_ID" --header "Authorization: Bearer $ACCESS_TOKEN"`
+function bak_get_existing_file_list {
+BAK_ARRAY_FILE=()
+local count_index=0
+local MY_ARRAY
+for BAK_FILE in "${BAKDIR}*.zip"; do
+	local BAK_FILESIZE=$(stat -c%s "$BAK_FILE")
+	local BAK_FILENAME=$(basename "$BAK_FILE")
+	MY_ARRAY='bak_id'$count_index'=('$BAK_FILE' '$BAK_FILENAME' '$BAK_FILESIZE')'
+	BAK_ARRAY_FILE=("${BAK_ARRAY_FILE[@]}" "$MY_ARRAY")
+	count_index=`expr $count_index + 1`
+done
 }
 
 function comp_gd_bak_file {
-count_index=0
+local count_index=0
 for i in "${GD_ARRAY_FILE[@]}"; do
         eval $i
         eval "local FILE=`echo '$BAKDIR${id'$count_index'[1]}'`"
@@ -160,16 +174,35 @@ ACCESS_TOKEN=${ACCESS_TOKEN}
 REFRESH_TOKEN=${REFRESH_TOKEN}
 EOL
 }
-if [ -r /usr/local/bin/.wpbak2gdrive.opt ]; then
-        . /usr/local/bin/.wpbak2gdrive.opt
+if [ -r /etc/default/gd_bakfile.conf ]; then
+	. /etc/default/gd_bakfile.conf
+	SCOPE=${SCOPE:-"https://docs.google.com/feeds"}
 else
-        create_opt
-        . /usr/local/bin/.wpbak2gdrive.opt
+	read -p "Enter Client ID :" CLIENT_ID
+	read -p "Enter Client Secret :" CLIENT_SECRET
+	read -p "Enter Google scope (or leave blank for default) :" SCOPE
+	read -p "Enter Directory to backup:" BAKDIR
+	read -p "Enter Google Directory destination (if not exist it will be created):" UPLOAD_DIR
+        cat >/usr/local/bin/.wpbak2gdrive.opt <<EOL
+BAKDIR="${BAKDIR}"
+UPLOAD_DIR_ID="$(gd_get_folder_id)"
+CLIENT_ID="${CLIENT_ID}"
+CLIENT_SECRET="${CLIENT_SECRET}"
+SCOPE=${SCOPE:-"https://docs.google.com/feeds"}
+EOL
 fi
-auth2 refresh $REFRESH_TOKEN
-refresh_opt
 
-gd_get_existing_file
+if [ -r /usr/local/bin/.wpbak2gdrive.opt ]; then
+	. /usr/local/bin/.wpbak2gdrive.opt
+	auth2 refresh $REFRESH_TOKEN
+	refresh_opt
+else
+	create_opt
+	. /usr/local/bin/.wpbak2gdrive.opt
+fi
+gd_get_existing_file_list
+bak_get_existing_file_list
+
 comp_gd_bak_file
 gd_get_existing_file
 comp_bak_gd_file
