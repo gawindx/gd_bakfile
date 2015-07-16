416b3a276dbb98fdac1135fb83fc3a585093ef78
gawindx <decauxnico@gmail.com>
2015-07-16 10:16:11 +0200

    Fixes Python function

--- gd_bakfile.sh
+++ gd_bakfile.sh
@@ -8,14 +8,9 @@
 
 set -e
 
-function prettyjson_test {
+function prettyjson {
 #Convert JSON data to human-readable form.
-#(Reads from stdin and writes to stdout)
-#local DATA=""
-#while read data; do
-#	DATA=$DATA$data
-#done
-PYTHON_ARG="$1" python - <<END
+PYTHON_ARG="$*" /usr/bin/python - <<END
 import os
 import simplejson as json
 json_data=os.environ['PYTHON_ARG']
@@ -37,7 +32,7 @@ class Unbuffered(object):
 	def __getattr__(self, attr):
 		return getattr(self.stream, attr)
 
-argv=os.environ['PYTHON_ARG'].split(' ')
+argv = os.environ['PYTHON_ARG'].split(' ')
 inputfile = ''
 chunck_size = 0
 skip_size = 0
@@ -55,7 +50,8 @@ for opt, arg in opts:
 	elif opt in ("-c", "--chunck_size"):
 		chunck_size = int(arg)
 	elif opt in ("-s", "--skip_size"):
-	skip_size = int(arg)
+		skip_size = int(arg)
+
 sys.stdout = Unbuffered(sys.stdout)
 f = open(inputfile,'rb',0)
 f.seek(skip_size,0)
@@ -66,47 +62,46 @@ END
 }
 
 function auth2 {
-# A simple cURL OAuth2 authenticator
-# depends on Python's built-in json module to prettify output
-#
-# Usage:
-#	./google-oauth2.sh create - authenticates a user
-#	./google-oauth2.sh refresh <token> - gets a new token
-#
+# inspired from https://github.com/soulseekah/bash-utils/tree/master/google-oauth2
 
 if [ "$1" == "create" ]; then
 	local RESPONSE=`curl --silent "https://accounts.google.com/o/oauth2/device/code" \
 		--data "client_id=$CLIENT_ID&scope=$SCOPE"`
-	local DEVICE_CODE=`echo "$RESPONSE" | prettyjson | grep 'device_code' | \
+	local DEVICE_CODE=`prettyjson "$RESPONSE" | grep 'device_code' | \
 		cut -d ':' -f 2 | sed 's/"//g' | sed 's/,//g' | sed 's/\ //g'`
-	local USER_CODE=`echo "$RESPONSE" | prettyjson | grep 'user_code' | \
+	local USER_CODE=`prettyjson "$RESPONSE" | grep 'user_code' | \
 		cut -d ':' -f 2 | sed 's/"//g' | sed 's/,//g' | sed 's/\ //g'`
-	local URL=`echo "$RESPONSE" | prettyjson | grep 'verification_url' | \
+	local URL=`prettyjson "$RESPONSE" | grep 'verification_url' | \
 		cut -d ':' -f 2 | sed 's/"//g' | sed 's/\ //g'`
-	URL="$URL:"`echo "$RESPONSE" | prettyjson | grep 'verification_url' | \
+	URL="$URL:"`prettyjson "$RESPONSE" | grep 'verification_url' | \
 		cut -d ':' -f 3 | sed 's/"//g' | sed 's/\ //g'`
-	local TOKEN_TTL=`echo "$RESPONSE" | prettyjson | grep 'expires_in' | \
+	local TOKEN_TTL=`prettyjson "$RESPONSE" | grep 'expires_in' | \
 		cut -d ':' -f 2  | sed 's/,//g'| sed 's/\ //g'`
 	echo -n "Go to $URL and enter $USER_CODE to grant access to this application. Hit enter when done..."
 	read -t $TOKEN_TTL ENTER_KEY
 	ENTER_KEY=${ENTER_KEY:-timeout}
-#	if [ "$ENTER_KEY" == "timeout" ]; then exit 1
+#       if [ "$ENTER_KEY" == "timeout" ]; then exit 1
+	DATA="client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET&code=$DEVICE_CODE"
+	DATA="$DATA&grant_type=http://oauth.net/grant_type/device/1.0"
 	RESPONSE=`curl --silent "https://accounts.google.com/o/oauth2/token" \
-		--data "client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET&code=$DEVICE_CODE&grant_type=http://oauth.net/grant_type/device/1.0"`
-	ACCESS_TOKEN=`echo "$RESPONSE" | prettyjson | grep 'access_token' | \
+		--data "$DATA"`
+	ACCESS_TOKEN=`prettyjson "$RESPONSE" | grep 'access_token' | \
 		cut -d ',' -f 1 | cut -d ':' -f 2 | sed 's/"//g' | sed 's/\ //g'`
-	REFRESH_TOKEN=`echo "$RESPONSE" | prettyjson | grep 'refresh_token' | \
+	REFRESH_TOKEN=`prettyjson "$RESPONSE" | grep 'refresh_token' | \
 		cut -d ',' -f 1 | cut -d ':' -f 2 | sed 's/"//g' | sed 's/\ //g'`
 elif [ "$1" == "refresh" ]; then
 	REFRESH_TOKEN="$2"
+	DATA="client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET"
+	DATA="$DATA&refresh_token=$REFRESH_TOKEN&grant_type=refresh_token"
 	local RESPONSE=`curl --silent "https://accounts.google.com/o/oauth2/token" \
-		--data "client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET&refresh_token=$REFRESH_TOKEN&grant_type=refresh_token"`
-	ACCESS_TOKEN=`echo "$RESPONSE" | prettyjson | grep 'access_token' | \
+		--data "$DATA"`
+	ACCESS_TOKEN=`prettyjson "$RESPONSE" | grep 'access_token' | \
 		cut -d ',' -f 1 | cut -d ':' -f 2 | sed 's/"//g' | sed 's/\ //g'`
 fi
 }
 
 function upload {
+# Inspired from https://github.com/soulseekah/bash-utils/blob/master/google-drive-upload/upload.sh
 local BOUNDARY=`cat /dev/urandom | head -c 16 | xxd -ps`
 local MIME_TYPE=${5:-"application/octet-stream"}
 local FULL_PATH="$2"
@@ -147,7 +142,6 @@ else
 		if [ "$END_BYTES" -gt "$FILESIZE" ]; then
 			END_BYTES=$((FILESIZE-1))
 		fi
-		chunck_file -i "$FULL_PATH" -c 128 -s 2
 		LENGTH=`expr $((END_BYTES+1)) - $START_BYTES`
 		local RESPONSE=""
 		local RETRY=0
@@ -164,7 +158,7 @@ else
 				--header "Authorization: Bearer $ACCESS_TOKEN" \
 				--header "Content-Length: 0" \
 				--header "Content-Range: bytes */$FILESIZE" 2>&1 | grep 'Range: bytes=' | cut -d '-' -f 2 \
-				| sed 's/\ //g'	| sed 's/\n//g'| sed 's/\r//g'`
+				| sed 's/\ //g' | sed 's/\n//g'| sed 's/\r//g'`
 			START_BYTES=`expr $UPLOADED_BYTES + 1`
 		elif [ ! -z "`echo "$RESPONSE" | grep '201'`" ]; then
 			break
@@ -173,7 +167,7 @@ else
 		elif [ ! -z "`echo "$RESPONSE" | grep '404'`" ]; then
 			break
 		else
-		   	RESPONSE=""
+			RESPONSE=""
 			local RETRY=0
 			while [ ! -z "`echo "$RESPONSE" | grep '308\|200\|201'`" ]||[ "$RETRY" -le "10" ]; do
 				local UPLOADED_BYTES=`curl -s -D- -X "PUT" "$SESSION_URI" \
@@ -190,7 +184,7 @@ else
 			if [ "$RETRY" -le "10" ]; then
 				START_BYTES=$((`echo "$RESPONSE" | grep 'Range:' | cut -d '-' -f 2` + 1))
 			else
-		    	break
+				break
 			fi
 		fi
 	done
@@ -202,7 +196,7 @@ local DESTDIR="$1"
 local QUERY="title%3D%27"$DESTDIR"%27%20and%20mimeType%3D%27application%2Fvnd.google-apps.folder%27"
 local RESPONSE=`curl --silent "https://www.googleapis.com/drive/v2/files/root/children?q=$QUERY" \
 	--header "Authorization: Bearer $ACCESS_TOKEN"`
-local DESTDIR_ID=`echo $RESPONSE | prettyjson | grep '"id":' | cut -d ':' -f 2  | sed 's/,//g'\
+local DESTDIR_ID=`prettyjson "$RESPONSE" | grep '"id":' | cut -d ':' -f 2  | sed 's/,//g'\
 	| sed 's/\ //g' | sed 's/"//g'`
 if [ -z "$DESTDIR_ID" ]; then
 	DESTDIR_ID=$(gd_create_destdir $DESTDIR)
@@ -220,7 +214,7 @@ local RESPONSE=`curl --silent -X "POST" "https://www.googleapis.com/drive/v2/fil
 	--header "Content-Type: application/json" \
 	--header "Content-Length: $DATA_SIZE" \
 	--data "$DATA_JSON"`
-local DESTDIR_ID=`echo $RESPONSE | prettyjson | grep -m 1 '"id":'| cut -d ':' -f 2 \
+local DESTDIR_ID=`prettyjson "$RESPONSE" | grep -m 1 '"id":'| cut -d ':' -f 2 \
 	| sed 's/,//g'| sed 's/\ //g' | sed 's/"//g' | cut -d ' ' -f 1`
 echo $DESTDIR_ID
 }
@@ -243,8 +237,8 @@ function gd_get_file_info {
 local GD_FILE_ID=$1
 local RESPONSE=`curl --silent "https://www.googleapis.com/drive/v2/files/$GD_FILE_ID" \
 	--header "Authorization: Bearer $ACCESS_TOKEN"`
-local GD_FILE_NAME=`echo "$RESPONSE" | prettyjson | grep "title" | cut -d '"' -f 4`
-local GD_FILE_SIZE=`echo "$RESPONSE" | prettyjson | grep "fileSize" | cut -d '"' -f 4`
+local GD_FILE_NAME=`prettyjson "$RESPONSE" | grep "title" | cut -d '"' -f 4`
+local GD_FILE_SIZE=`prettyjson "$RESPONSE" | grep "fileSize" | cut -d '"' -f 4`
 local ARRAY_GD_INFO_FILE=("$GD_FILE_ID" "$GD_FILE_NAME" "$GD_FILE_SIZE")
 echo ${ARRAY_GD_INFO_FILE[*]}
 }
@@ -253,7 +247,7 @@ function gd_get_existing_file_list {
 GD_ARRAY_FILE=()
 local RESPONSE=`curl --silent "https://www.googleapis.com/drive/v2/files/$UPLOAD_DIR_ID/children?q=trashed%3Dfalse" \
 	--header "Authorization: Bearer $ACCESS_TOKEN"`
-local GD_FILE_LIST=`echo "$RESPONSE" | prettyjson | grep "id" | cut -d '"' -f 4`
+local GD_FILE_LIST=`prettyjson "$RESPONSE" | grep "id" | cut -d '"' -f 4`
 local count_index=0
 local MY_ARRAY
 for file_id in $GD_FILE_LIST; do
@@ -287,7 +281,7 @@ if [ -r /etc/default/gd_bakfile.conf ]; then
 	. /etc/default/gd_bakfile.conf
 	SCOPE=${SCOPE:-"https://docs.google.com/feeds"}
 else
-   	read -p "Enter Client ID :" CLIENT_ID
+	read -p "Enter Client ID :" CLIENT_ID
 	read -p "Enter Client Secret :" CLIENT_SECRET
 	read -p "Enter Google scope (or leave blank for default) :" SCOPE
 	read -p "Enter Directory to backup:" BAKDIR
@@ -298,7 +292,7 @@ if [ -r /usr/local/bin/.gd_bakfile.opt ]; then
 	auth2 refresh $REFRESH_TOKEN
 	refresh_opt
 else
-   	create_opt
+	create_opt
 	. /usr/local/bin/.gd_bakfile.opt
 fi
 if [ ! -z "$UPLOAD_DIR" ] && [ -z "$UPLOAD_DIR_ID" ]; then
@@ -337,7 +331,7 @@ for file in `ls ${BAKDIR}*.zip`; do
 			if [ "$BAK_FILESIZE" -ne "$GD_FILESIZE" ]; then
 				gd_delete_file "$GD_FILEID"
 			else
-		    	DO_BAK="0"
+				DO_BAK="0"
 			fi
 		fi
 	done
